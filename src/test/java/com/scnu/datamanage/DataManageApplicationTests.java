3d51ec7f785fbeabda0be70a92c9ea331c14276f
369pro <Agenda1462553103@outlook.com>
2024-10-05 22:58:05 +0800

    first commit

---
+++ src/test/java/com/scnu/datamanage/DataManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scnu.datamanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
